5ae7c64a890ee8f188c1317b58d4da8dc3da3924
priyanshu <priyanshuarya17@gmail.com>
2023-06-07 16:21:06 +0530

    Add project files.

---
+++ ArmStrongNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmStrongNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write($"Enter the number to check it is armstrong or not :- ");
+            int num = int.Parse(Console.ReadLine());
+
+            int temp = num, sum = 0, rem;
+            while (temp > 0)
+            {
+                rem = temp % 10;
+                sum = sum + (rem * rem * rem);
+                temp = temp / 10;
+            }
+           var result=  sum == num ? $"{num} is a armstrong number " : $"{num} is not a armstrong number";
+            Console.WriteLine(result);
+            Console.ReadLine(); 
+        }
+    }
+}
